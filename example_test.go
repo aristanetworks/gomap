f259188b61aaa7b5d7ab91f6a477b9b61db3fe90
Aaron Beitch <aaronb@arista.com>
2022-09-02 15:42:04 -0700

    Add example of []byte-keyed map and map[string]struct{}-keyed map

--- example_test.go
+++ example_test.go
@@ -5,6 +5,7 @@
 package gomap_test
 
 import (
+	"bytes"
 	"fmt"
 	"hash/maphash"
 
@@ -23,4 +24,36 @@ func ExampleMap_Iter() {
 	for i := m.Iter(); i.Next(); {
 		fmt.Printf("The abbreviation for %q is %q", i.Key(), i.Elem())
 	}
+}
+
+func ExampleNew() {
+	// Some examples of different maps:
+
+	// Map that uses []byte as the key type
+	byteSliceMap := gomap.New[[]byte, int](bytes.Equal, maphash.Bytes)
+	byteSliceMap.Set([]byte("hello"), 42)
+
+	// Map that uses map[string]struct{} as the key type
+	stringSetEqual := func(a, b map[string]struct{}) bool {
+		if len(a) != len(b) {
+			return false
+		}
+		for k := range a {
+			_, ok := b[k]
+			if !ok {
+				return false
+			}
+		}
+		return true
+	}
+	stringSetHash := func(seed maphash.Seed, ss map[string]struct{}) uint64 {
+		var sum uint64
+		for s := range ss {
+			// combine hashes with addition so that iteration order does not matter
+			sum += maphash.String(seed, s)
+		}
+		return sum
+	}
+	stringSetMap := gomap.New[map[string]struct{}, int](stringSetEqual, stringSetHash)
+	stringSetMap.Set(map[string]struct{}{"foo": {}, "bar": {}}, 42)
 }
