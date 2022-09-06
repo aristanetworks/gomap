c62d8c7e0bf457127e138f1962408297eda47ae9
Aaron Beitch <aaronb@arista.com>
2022-09-06 14:53:24 -0700

    Update copyright statements

--- map_test.go
+++ map_test.go
@@ -1,3 +1,5 @@
+// Modifications copyright (c) Arista Networks, Inc. 2022
+// Underlying
 // Copyright 2014 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
