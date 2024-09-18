d3483231a225e03776f4b87a5b9db902ca8c96d8
Aaron Beitch <aaronb@arista.com>
2024-09-18 14:16:45 -0700

    Reimplement Map.All without using Map.Iter
    Implement the native iterate code inside All. All should be the
    preferred way to iterate over Map when using go1.23. This
    reimplementation reduces its overhead.
    
    goos: darwin
    goarch: amd64
    pkg: github.com/aristanetworks/gomap
    cpu: Intel(R) Core(TM) i9-9980HK CPU @ 2.40GHz
           │  before.txt  │              after.txt              │
           │    sec/op    │   sec/op     vs base                │
    All-16   157.60n ± 2%   56.70n ± 1%  -64.02% (p=0.000 n=10)
    
           │ before.txt │             after.txt              │
           │    B/op    │   B/op     vs base                 │
    All-16   96.00 ± 0%   0.00 ± 0%  -100.00% (p=0.000 n=10)
    
           │ before.txt │              after.txt              │
           │ allocs/op  │ allocs/op   vs base                 │
    All-16   1.000 ± 0%   0.000 ± 0%  -100.00% (p=0.000 n=10)

--- iter_test.go
+++ iter_test.go
@@ -11,6 +11,7 @@ package gomap
 import (
 	"hash/maphash"
 	"maps"
+	"sync"
 	"testing"
 )
 
@@ -91,4 +92,190 @@ func TestRangeFuncs(t *testing.T) {
 			t.Errorf("expected: %v got: %v", exp, got)
 		}
 	})
+}
+
+func TestGetAllRace(t *testing.T) {
+	m := NewHint[int, int](100, func(a int, b int) bool { return a == b }, intHash)
+	for i := 0; i < 100; i++ {
+		m.Set(i, i)
+	}
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		for i := 0; i < 100; i++ {
+			v, ok := m.Get(i)
+			if !ok || v != i {
+				t.Errorf("expected: %d got: %d, %t", i, v, ok)
+			}
+		}
+		wg.Done()
+	}()
+	wg.Add(1)
+	go func() {
+		for i := 0; i < 100; i++ {
+			v, ok := m.Get(i)
+			if !ok || v != i {
+				t.Errorf("expected: %d got: %d, %t", i, v, ok)
+			}
+		}
+		wg.Done()
+	}()
+
+	wg.Add(1)
+	go func() {
+	outer:
+		for i := 0; i < 100; i++ {
+			for range m.All() {
+				continue outer
+			}
+			t.Error("Should have iterated at least once, but didn't.")
+		}
+		wg.Done()
+	}()
+	wg.Add(1)
+	go func() {
+	outer:
+		for i := 0; i < 100; i++ {
+			for range m.All() {
+				continue outer
+			}
+			t.Error("Should have iterated at least once, but didn't.")
+		}
+		wg.Done()
+	}()
+	wg.Wait()
+}
+
+func TestAll(t *testing.T) {
+	m := New[uint64, uint64](
+		func(a, b uint64) bool { return a == b },
+		badIntHash,
+	)
+	expected := make(map[uint64]uint64, 9)
+	for i := uint64(0); i < 9; i++ {
+		expected[i] = i
+		m.Set(i, i)
+	}
+	for k, v := range m.All() {
+		e, ok := expected[k]
+		if !ok {
+			t.Errorf("unexpected value in m: [%d: %d]", k, v)
+			continue
+		}
+		if e != v {
+			t.Errorf("wrong value for key %d. Expected: %d Got: %d", k, e, v)
+			continue
+		}
+		delete(expected, k)
+	}
+	if len(expected) > 0 {
+		t.Errorf("Values not found in m: %v", expected)
+	}
+}
+
+func TestAllDuringResize(t *testing.T) {
+	m := New[uint64, uint64](
+		func(a, b uint64) bool { return a == b },
+		badIntHash,
+	)
+
+	// insert numbers that initially hash to the same bucket, but will
+	// be split into different buckets on resize. Evens will end up in
+	// bucket[0], odds end up in bucket[1] thanks to badIntHash.
+	initial := map[uint64]uint64{0: 0, 1: 1, 2: 2, 3: 3}
+	for k, e := range initial {
+		m.Set(k, e)
+	}
+	additional := map[uint64]uint64{100: 100, 101: 101, 102: 102, 103: 103, 104: 104}
+
+	first := true
+	// start the iter
+	for k, v := range m.All() {
+		if first {
+			first = false
+			// Add some additional data to cause a resize
+			for k, e := range additional {
+				m.Set(k, e)
+			}
+			// Remove 1 value that in each of the initial and split
+			// buckets that we haven't seen yet
+			if k == 0 {
+				m.Delete(2)
+				delete(initial, 2)
+			} else {
+				m.Delete(0)
+				delete(initial, 0)
+			}
+			if k == 1 {
+				m.Delete(3)
+				delete(initial, 3)
+			} else {
+				m.Delete(1)
+				delete(initial, 1)
+			}
+		}
+		if k != v {
+			t.Errorf("expected key == elem, but got: %d != %d", k, v)
+			t.Error(m.debugString())
+		}
+		if _, ok := initial[k]; ok {
+			delete(initial, v)
+			continue
+		}
+		if _, ok := additional[k]; ok {
+			t.Logf("Saw key from additional: %d", k)
+			continue
+		}
+		t.Errorf("Unexpected value from iter: %d", k)
+	}
+	for k := range initial {
+		t.Errorf("iter missing key: %d", k)
+	}
+}
+
+func TestAllDuringGrow(t *testing.T) {
+	m := New[uint64, uint64](
+		func(a, b uint64) bool { return a == b },
+		badIntHash,
+	)
+
+	// Insert exactly 27 numbers so we end up in the middle of a grow.
+	expected := make(map[uint64]uint64, 27)
+	for i := uint64(0); i < 27; i++ {
+		expected[i] = i
+		m.Set(i, i)
+	}
+
+	for k, v := range m.All() {
+		t.Logf("Key: %d", k)
+		if k != v {
+			t.Errorf("expected key == elem, but got: %d != %d", k, v)
+			t.Error(m.debugString())
+		}
+
+		if _, ok := expected[k]; ok {
+			delete(expected, v)
+			continue
+		}
+		t.Errorf("Unexpected value from iter: %d", k)
+	}
+	for k := range expected {
+		t.Errorf("iter missing key: %d", k)
+	}
+}
+
+func BenchmarkAll(b *testing.B) {
+	m := New[string, int](
+		func(a, b string) bool { return a == b },
+		maphash.String,
+		KeyElem[string, int]{"one", 1},
+		KeyElem[string, int]{"two", 2},
+		KeyElem[string, int]{"three", 3},
+	)
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		for range m.All() {
+		}
+	}
 }
